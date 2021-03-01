4d39096562368dc6347aba2cf6e613b3677e5319
Siarhei Burakouski <serge.burakovsky@gmail.com>
2021-03-01 17:52:33 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
 module.exports = function toReadable (number) {
-  
+    if (number < 20) return num[number];
+    var digit = number%10;
+    if (number < 100) return tens[Math.floor(number/10)-2] + (digit? " " + num[digit]: "");
+    if (number < 1000) return num[Math.floor(number/100)] +" hundred" + (number%100 == 0? "": " " + toReadable(number%100));
+
 }
